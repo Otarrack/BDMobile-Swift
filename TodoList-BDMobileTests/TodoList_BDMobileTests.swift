a1167995305af155a1ddf6be876317e38f10163a
Florian Van Den Berghe <vandenber.florian@gmail.com>
2018-03-30 09:33:38 +0200

    Initial Commit

---
+++ TodoList-BDMobileTests/TodoList_BDMobileTests.swift
@@ -0,0 +1,36 @@
+//
+//  TodoList_BDMobileTests.swift
+//  TodoList-BDMobileTests
+//
+//  Created by Florian Van Den Berghe on 30/03/2018.
+//  Copyright © 2018 Florian Van Den Berghe. All rights reserved.
+//
+
+import XCTest
+@testable import TodoList_BDMobile
+
+class TodoList_BDMobileTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
